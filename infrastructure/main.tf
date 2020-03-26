ee2ca5cbb5f828e658f4a3f0ef129d18a0cecd8e
Dave Jones <38425925+davejones74@users.noreply.github.com>
2020-03-26 09:46:04 +0000

    Removed webapp module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,34 +23,6 @@ module "pcq-db" {
   subscription           = "${var.subscription}"
 }
 
-module "pcq" {
-  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product                         = "${var.product}-${var.component}"
-  location                        = "${var.location}"
-  env                             = "${var.env}"
-  java_container_version          = "9.0"
-  subscription                    = "${var.subscription}"
-  common_tags                     = "${var.common_tags}"
-  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
-  is_frontend                     = "false"
-  asp_name                        = "${local.asp_name}"
-  asp_rg                          = "${local.asp_name}"
-
-  app_settings = {
-    // db
-    PCQ_DB_HOST                   = "${module.pcq-db.host_name}"
-    PCQ_DB_PORT                   = "${module.pcq-db.postgresql_listen_port}"
-    PCQ_DB_USERNAME               = "${module.pcq-db.user_name}"
-    PCQ_DB_PASSWORD               = "${module.pcq-db.postgresql_password}"
-    PCQ_DB_NAME                   = "${module.pcq-db.postgresql_database}"
-    PCQ_DB_CONN_OPTIONS           = "${local.db_connection_options}"
-    FLYWAY_URL                    = "jdbc:postgresql://${module.pcq-db.host_name}:${module.pcq-db.postgresql_listen_port}/${module.pcq-db.postgresql_database}${local.db_connection_options}"
-    FLYWAY_USER                   = "${module.pcq-db.user_name}"
-    FLYWAY_PASSWORD               = "${module.pcq-db.postgresql_password}"
-    FLYWAY_NOOP_STRATEGY          = "true"
-  }
-}
-
 data "azurerm_key_vault" "key_vault" {
   name                = "${local.vault_name}"
   resource_group_name = "${local.vault_name}"
@@ -62,31 +34,31 @@ data "azurerm_key_vault" "key_vault" {
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "${var.component}-POSTGRES-USER"
+  name         = "${var.product}-POSTGRES-USER"
   value        = "${module.pcq-db.user_name}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "${var.component}-POSTGRES-PASS"
+  name         = "${var.product}-POSTGRES-PASS"
   value        = "${module.pcq-db.postgresql_password}"
 }
   
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "${var.component}-POSTGRES-HOST"
+  name         = "${var.product}-POSTGRES-HOST"
   value        = "${module.pcq-db.host_name}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "${var.component}-POSTGRES-PORT"
+  name         = "${var.product}-POSTGRES-PORT"
   value        = "${module.pcq-db.postgresql_listen_port}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "${var.component}-POSTGRES-DATABASE"
+  name         = "${var.product}-POSTGRES-DATABASE"
   value        = "${module.pcq-db.postgresql_database}"
 }
 
