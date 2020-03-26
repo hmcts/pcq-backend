db14a28c8da22d4033034fbddbd015077e12e5ec
Manoj Kulkarni <manoj.kulkarni@cgi.com>
2020-03-26 09:59:24 +0000

    Added integ test to cover the jacoco report

--- src/integrationTest/java/uk/gov/hmcts/reform/pcqbackend/controllers/CreatePcqRequestTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pcqbackend/controllers/CreatePcqRequestTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.system.OutputCaptureRule;
 import uk.gov.hmcts.reform.pcqbackend.domain.ProtectedCharacteristics;
@@ -26,8 +27,10 @@ public class CreatePcqRequestTest extends PcqIntegrationTest {
     public static final String RESPONSE_KEY_2 = "responseStatusCode";
     public static final String RESPONSE_KEY_3 = "responseStatus";
     public static final String HTTP_CREATED = "201";
+    public static final String HTTP_INTERNAL_ERROR = "500";
     public static final String RESPONSE_CREATED_MSG = "Successfully created";
     public static final String TEST_PCQ_ID = "Integ-Test-1";
+    public static final String RESPONSE_UNKNOWN = "Unknown error occurred";
 
 
     @Rule
@@ -86,6 +89,32 @@ public class CreatePcqRequestTest extends PcqIntegrationTest {
             log.error("IOException while executing test", e);
         }
 
+
+    }
+
+    /**
+     * This method tests the submitAnswers API when it is called with all valid parameters but the database call
+     * returns an exception. The response status code will be 500.
+     */
+    @DisplayName("Should return with an 500 error code for transaction error.")
+    @Test
+    public void testControllerInternalError()  {
+
+        try {
+            String jsonStringRequest = jsonStringFromFile("JsonTestFiles/FirstSubmitAnswer.json");
+            PcqAnswerRequest answerRequest = jsonObjectFromString(jsonStringRequest);
+            answerRequest.setPcqId("");
+            //logger.info("testSubmitAnswersFirstTime - Generated Json String is " + jsonStringRequest);
+
+            Map<String, Object> response = pcqBackEndClient.createPcqAnswer(answerRequest);
+
+            assertEquals("Response Status Code not valid", null, response.get(RESPONSE_KEY_2));
+
+            checkLogsForKeywords();
+
+        } catch (Exception e) {
+            log.error("IOException while executing test", e);
+        }
 
     }
 
