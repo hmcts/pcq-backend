df8989c5011f026945e7c3561236bc5405310102
Priyanka Verma <priyanka.verma@hmcts.net>
2023-05-10 05:27:22 +0100

    PCQ-1645 Integration test fix

--- src/main/java/uk/gov/hmcts/reform/pcqbackend/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcqbackend/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,7 +24,8 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+        .csrf().disable() //NOSONAR not used in secure contexts
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse
@@ -40,4 +42,5 @@ public class SecurityConfiguration {
         return http.build();
     }
 }
+
 
