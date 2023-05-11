5cf7b79765891dd7d2ea2d5381d722e511d8d450
Priyanka Verma <priyanka.verma@hmcts.net>
2023-05-11 11:54:23 +0100

    PCQ-1645 refactor code

--- src/test/java/uk/gov/hmcts/reform/pcqbackend/config/SwaggerGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcqbackend/config/SwaggerGeneratorTest.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,8 +22,6 @@ import java.nio.file.Paths;
 @SpringJUnitWebConfig
 @SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration
-@WebAppConfiguration
 class SwaggerGeneratorTest {
 
     @Autowired
