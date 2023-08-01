929dd7d774997c6f0a4eb5de7bb96f262005fabc
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-08-01 10:26:43 +0100

    PCQ-1783 address CVE-2023-34035

--- src/main/java/uk/gov/hmcts/reform/pcqbackend/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcqbackend/security/SecurityConfiguration.java
@@ -3,15 +3,17 @@ package uk.gov.hmcts.reform.pcqbackend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import javax.servlet.http.HttpServletResponse;
 import java.security.Security;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -22,38 +24,43 @@ public class SecurityConfiguration {
     @Autowired
     private JwtConfiguration jwtConfiguration;
 
+    @Autowired
+    HandlerMappingIntrospector introspector;
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers(
-            "/swagger-ui.html",
-            "/webjars/springfox-swagger-ui/**",
-            "/swagger-resources/**",
-            "/health",
-            "/health/liveness",
-            "/health/readiness",
-            "/v2/api-docs/**",
-            "/info",
-            "/favicon.ico",
-            "/"
+            new MvcRequestMatcher(introspector, "/swagger-ui.html"),
+            new MvcRequestMatcher(introspector, "/webjars/springfox-swagger-ui/**"),
+            new MvcRequestMatcher(introspector, "/swagger-resources/**"),
+            new MvcRequestMatcher(introspector, "/health"),
+            new MvcRequestMatcher(introspector, "/health/liveness"),
+            new MvcRequestMatcher(introspector, "/health/readiness"),
+            new MvcRequestMatcher(introspector, "/v2/api-docs/**"),
+            new MvcRequestMatcher(introspector, "/info"),
+            new MvcRequestMatcher(introspector, "/favicon.ico"),
+            new MvcRequestMatcher(introspector, "/")
         );
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable()) //NOSONAR not used in secure contexts
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(csrf -> csrf.disable()) //NOSONAR not used in secure contexts
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-        .exceptionHandling(exc -> exc.authenticationEntryPoint(
-            (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
-        .addFilterAfter(new JwtTokenFilter(jwtConfiguration), UsernamePasswordAuthenticationFilter.class)
-        .authorizeRequests()
-        .requestMatchers("/pcq/backend/getAnswer/**").permitAll()
-        .requestMatchers("/pcq/backend/consolidation/**").permitAll()
-        .requestMatchers("/pcq/backend/token/**").permitAll()
-        .requestMatchers("/pcq/backend/deletePcqRecord/**").permitAll()
-        .requestMatchers("/pcq/backend/submitAnswers**").authenticated()
-        .requestMatchers("/v2/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
+            .exceptionHandling(exc -> exc.authenticationEntryPoint(
+                (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+            .addFilterAfter(new JwtTokenFilter(jwtConfiguration), UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/pcq/backend/getAnswer/**")).permitAll()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/pcq/backend/consolidation/**")).permitAll()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/pcq/backend/token/**")).permitAll()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/pcq/backend/deletePcqRecord/**")).permitAll()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/pcq/backend/submitAnswers**")).authenticated()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/v2/api-docs/**")).permitAll()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/swagger-ui/**")).permitAll()
+            .requestMatchers(new MvcRequestMatcher(introspector, "/swagger-ui.html")).permitAll();
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
         return http.build();
     }
