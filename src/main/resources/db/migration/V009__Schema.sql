fb17e71d61883b5955a648df04d6f0a7c36d00a2
Martin <martin.spasov@hmcts.net>
2024-06-18 12:01:03 +0100

    Remove pgcrypto script

--- src/main/resources/db/migration/V009__Schema.sql
+++
@@ -1,5 +0,0 @@
--- Check if pgcrypto is already installed
-SELECT extname FROM pg_extension WHERE extname = 'pgcrypto';
-
--- If not installed, install pgcrypto
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
