4a465a96b1a02f4637b7539aaac4179140778982
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2024-06-25 10:46:45 +0100

    test

--- src/functionalTest/java/uk/gov/hmcts/reform/pcqbackend/service/PcqDisposerServiceFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcqbackend/service/PcqDisposerServiceFunctionalTest.java
@@ -11,7 +11,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureRule;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.pcqbackend.Application;
