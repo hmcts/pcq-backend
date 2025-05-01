b489ab996e25af91c2178fce679f805342467e9c
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-05-01 10:59:09 +0100

    CME-130 add prod db monitoring

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,6 +15,12 @@ locals {
   db_connection_options = "?sslmode=require"
   vault_name            = "${var.product}-${var.env}"
   asp_name              = "${var.product}-${var.env}"
+  db_server_name        = "${var.product}-${var.component}-flexible"
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = local.vault_name
+  resource_group_name = local.vault_name
 }
 
 module "pcq-db-flexible" {
@@ -28,13 +34,17 @@ module "pcq-db-flexible" {
   product       = var.product
   component     = var.component
   business_area = "cft"
-  name          = "${var.product}-${var.component}-flexible"
+  name          = local.db_server_name
 
   common_tags = var.common_tags
 
   pgsql_admin_username = "pcquser"
   pgsql_version        = "15"
   auto_grow_enabled    = true
+
+  action_group_name           = join("-", [var.db_monitor_action_group_name, local.db_server_name, var.env])
+  email_address_key           = var.db_alert_email_address_key
+  email_address_key_vault_id  = data.azurerm_key_vault.key_vault.id
 
   # Setup Access Reader db user
   force_user_permissions_trigger = "1"
@@ -53,12 +63,6 @@ module "pcq-db-flexible" {
   ]
 
   admin_user_object_id = var.jenkins_AAD_objectId
-}
-
-
-data "azurerm_key_vault" "key_vault" {
-  name                = local.vault_name
-  resource_group_name = local.vault_name
 }
 
 ////////////////////////////////
