bc4488dcd851e60ac02094465e175e40fbec8dd7
Priyanka Verma <priyanka.verma@hmcts.net>
2026-01-06 12:29:53 +0000

    FlywayConfiguration

--- src/main/java/uk/gov/hmcts/reform/pcqbackend/config/FlywayConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcqbackend/config/FlywayConfiguration.java
@@ -1,26 +1,13 @@
 package uk.gov.hmcts.reform.pcqbackend.config;
 
-
 import org.flywaydb.core.Flyway;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.pcqbackend.util.FlywayNoOpStrategy;
 
-@AutoConfigureAfter({
-    DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
-})
-@AutoConfigureBefore({
-    FlywayAutoConfiguration.class
-})
 @Configuration
 @ConditionalOnClass(Flyway.class)
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
@@ -35,6 +22,9 @@ public class FlywayConfiguration {
     @Bean
     @ConditionalOnProperty(prefix = "flyway.noop", name = "strategy", havingValue = "false")
     public FlywayMigrationStrategy flywayVoidMigrationStrategy() {
-        return null;
+        return flyway -> {
+            // do nothing (explicit no-op)
+        };
     }
 }
+
