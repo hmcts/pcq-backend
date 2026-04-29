6c7371bb0919b6b3a50f16831d93162efa7d3d8e
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-04-29 11:39:03 +0100

    PCQ-2250 replace deprecated tf argument

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 
 provider "azurerm" {
   subscription_id            = var.aks_subscription_id
-  skip_provider_registration = "true"
+  resource_provider_registrations = "none"
   features {}
   alias = "postgres_network"
 
