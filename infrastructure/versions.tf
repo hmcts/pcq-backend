44f1176a12e94ad7054a3c5e6745cbe6a8b04479
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-07-01 15:29:37 +0100

    PCQ-2251 address terraform observation

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,3 +1,17 @@
 terraform {
-  required_version = ">= 0.13"
+  backend "azurerm" {}
+
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.79.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.9.0"
+    }
+  }
 }
